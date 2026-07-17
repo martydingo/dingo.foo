d4d935b31f54f22c98cffaaa1534802b37b1d9b1
Martin George <marty@dingo.foo>
2026-07-17 14:57:24 +1000

    Nulled NextJS default pages

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "dingo.foo devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          bun
+          nodejs
+        ];
+      };
+    };
+}
